009291fa9b3257c9322f1919c5472acf1731da26
Сергей Соболев <sergey.sobolev@megafon.ru>
2021-11-23 23:39:17 +0300

    Частично внёс правки

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,6 +1,6 @@
-from django.db import models
 from django.contrib.auth import get_user_model
-# Create your models here.
+from django.db import models
+
 User = get_user_model()
 
 
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,14 +1,12 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Post, Group
-# Create your views here.
+from django.shortcuts import get_object_or_404, render
+
+from .models import Group, Post
 
 
 def index(request):
     template = 'posts/index.html'
-    title = 'Это главная страница проекта Yatube'
     posts = Post.objects.order_by('-pub_date')[:10]
     context = {
-        'title': title,
         'posts': posts
     }
     return render(request, template, context)
@@ -16,11 +14,9 @@ def index(request):
 
 def group_posts(request, slug):
     template = 'posts/group_list.html'
-    title = 'Здесь будет информация о группах проекта Yatube'
     group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
-        'title': title,
         'group': group,
         'posts': posts,
     }
