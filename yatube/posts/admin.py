c030a81d231f79b12004bce223348422b9d36e14
Сергей Соболев <sergey.sobolev@megafon.ru>
2021-11-24 21:55:51 +0300

    добавил правки

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -16,5 +16,14 @@ class PostAdmin(admin.ModelAdmin):
     empty_value_display = '-пусто-'
 
 
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('title',
+                    'description'
+                    )
+    list_editable = ('description',)
+    search_fields = ('title',)
+    empty_value_display = '-пусто-'
+
+
 admin.site.register(Post, PostAdmin)
-admin.site.register(Group)
+admin.site.register(Group, GroupAdmin)
