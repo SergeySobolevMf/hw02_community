d61c754cd0da4215578fc6478a451f0312666c8e
Сергей Соболев <sergey.sobolev@megafon.ru>
2021-12-08 07:55:07 +0300

    проект до валидации форм

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import get_object_or_404, render
-
+from django.core.paginator import Paginator
 from .models import Group, Post
 
 
 def index(request):
     template = 'posts/index.html'
-    posts = Post.objects.all()[:10]
+    posts = Post.objects.all().order_by('-pub_date')
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
-        'posts': posts
+        'posts': page_obj
     }
     return render(request, template, context)
 
--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'posts.apps.PostsConfig',
+    'users.apps.UsersConfig',
+    'core.apps.CoreConfig'
 ]
 
 MIDDLEWARE = [
@@ -64,6 +66,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'core.context_processors.year.year'
             ],
         },
     },
@@ -105,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
@@ -115,7 +118,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+LOGIN_URL = 'users:login'
+LOGIN_REDIRECT_URL = 'posts:index'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
